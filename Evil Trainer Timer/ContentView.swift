1885da480ac95a648bbba24a3911e1fdcd4c7a90
Scott J. Kleper <s@klep.me>
2024-08-26 14:55:11 -0400

    Initial Commit

---
+++ Evil Trainer Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Evil Trainer Timer
+//
+//  Created by Scott J. Kleper on 8/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Evil Trainer Timer/Evil_Trainer_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Evil_Trainer_TimerApp.swift
+//  Evil Trainer Timer
+//
+//  Created by Scott J. Kleper on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Evil_Trainer_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
